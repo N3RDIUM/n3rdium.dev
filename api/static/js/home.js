98150f5c0705bc7f4bfc7180cb10514083965736
n3rdium <leenagajbhiye1003@gmail.com>
2023-06-07 23:37:39 +0530

    ...

--- api/static/js/home.js
+++ api/static/js/home.js
@@ -1,7 +1,7 @@
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
-    // document.body.style.filter = "blur(" + Math.abs(e.velocity / 10) + "px)"
+    document.body.style.filter = "blur(" + Math.abs(e.velocity / 32) + "px)"
     document.getElementById("panel1").style.backgroundPositionY = -e.animatedScroll / window.innerHeight * 80 + "px"
 })
 
