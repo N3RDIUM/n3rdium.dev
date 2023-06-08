d215e2bf23252cfa75def537a125bcde47cfe697
n3rdium <leenagajbhiye1003@gmail.com>
2023-06-08 14:41:59 +0530

    To add skill icons

--- api/static/js/home.js
+++ api/static/js/home.js
@@ -1,8 +1,19 @@
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
+    document.getElementById("avatar").style.opacity = 1 - e.animatedScroll / window.innerHeight
+    document.getElementById("username").style.opacity = 1.1 - e.animatedScroll / window.innerHeight
+    document.getElementById("userdesc").style.opacity = 1.2 - e.animatedScroll / window.innerHeight
+    document.getElementById("sdi").style.opacity = 1.3 - e.animatedScroll / window.innerHeight
+
     document.body.style.filter = "blur(" + Math.abs(e.velocity / 32) + "px)"
     document.getElementById("panel1").style.backgroundPositionY = -e.animatedScroll / window.innerHeight * 80 + "px"
+    
+    document.getElementById("p1-head").style.opacity = e.animatedScroll / window.innerHeight
+    document.getElementById("p1-txt").style.opacity = e.animatedScroll / window.innerHeight - 0.1
+
+    document.getElementById("p2-head").style.opacity = e.animatedScroll / window.innerHeight - 1
+    document.getElementById("p2-txt").style.opacity = e.animatedScroll / window.innerHeight - 1.1
 })
 
 function raf(time) {
--- api/static/js/uname.js
+++ api/static/js/uname.js
@@ -6,28 +6,32 @@ if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
     el.classList.remove("uname")
 }
 
-let _chars = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM"
-let el = document.getElementsByClassName("uname-letter")
-let progress = []
-let toreach = []
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-function resetText(){
-    for (let i = 0; i < el.length; i++) {
-        progress.push(Math.abs(Math.floor(Math.random() * 16)))
-        toreach.push(el[i].innerHTML)
-        let interval = setInterval(async function() {
-            if(_chars[progress[i]] == toreach[i]) clearInterval(interval)
-            el[i].innerHTML = _chars[progress[i]]
-            progress[i]++
-            if(_chars[progress[i]] == undefined) {
-                progress[i] = 0
-            }
-        }, 10)
-    }
+let interval = null;
+const qs = ".uname"
+
+document.querySelector(qs).onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
 }
-
-// When the document is loaded, we will start the animation
-document.getElementById("username").addEventListener("click", resetText)
-document.getElementById("username").addEventListener("mouseenter", function(){
-    resetText()
-})
