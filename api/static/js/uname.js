23798788fa0a0acd1dd891fe429c3838a7aad2b1
n3rdium <leenagajbhiye1003@gmail.com>
2023-06-08 21:07:32 +0530

    ...

--- api/static/js/uname.js
+++ api/static/js/uname.js
@@ -10,28 +10,29 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 const qs = ".uname"
-
-document.querySelector(qs).onmouseover = event => {  
-    let iteration = 0;
-    
-    clearInterval(interval);
-    
-    interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter, index) => {
-          if(index < iteration) {
-            return event.target.dataset.value[index];
-          }
+try{
+  document.querySelector(qs).onmouseover = event => {  
+      let iteration = 0;
+      
+      clearInterval(interval);
+      
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return event.target.dataset.value[index];
+            }
+          
+            return letters[Math.floor(Math.random() * 26)]
+          })
+          .join("");
         
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
-      
-      if(iteration >= event.target.dataset.value.length){ 
-        clearInterval(interval);
-      }
-      
-      iteration += 1 / 3;
-    }, 30);
-}
+        if(iteration >= event.target.dataset.value.length){ 
+          clearInterval(interval);
+        }
+        
+        iteration += 1 / 3;
+      }, 30);
+  }
+} catch (e) {console.log(e)}
