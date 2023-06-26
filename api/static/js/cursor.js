cb448d8f9ae9d24182e632cf41a15873716ef2a5
N3RDIUM <leenagajbhiye1003@gmail.com>
2023-06-26 17:24:38 +0530

    Revert, add better cursor

--- api/static/js/cursor.js
+++ api/static/js/cursor.js
@@ -15,7 +15,7 @@ function animate() {
     current.y += (actual.y - current.y) * ease;
     let rotationZ = (current.y - previous.y) * (current.y - previous.y) / 20;
     let rotationY = (current.x - previous.x) * (current.x - previous.x) / 20;
-    document.querySelector('.cursor').style.transform = `translate3d(${current.x-20.5}px, ${current.y-20.5}px, 0) rotateX(${rotationZ}deg) rotateY(${rotationY}deg)`;
+    document.querySelector('.cursor').style.transform = `translate3d(${current.x-20.5}px, ${current.y-20.5+window.scrollY}px, 0) rotateX(${rotationZ}deg) rotateY(${rotationY}deg)`;
     frame += 1
     previous.x = temp_x;
     previous.y = temp_y;
