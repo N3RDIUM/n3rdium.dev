51eb23b5892949878995ccc5954286e7419b54b7
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-18 21:53:31 +0530

    ...

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -2,6 +2,33 @@ screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrient
 screen.lockOrientationUniversal("portrait-primary");
 screen.orientation.lock("portrait-primary");
 screen.lockOrientation("portrait-primary");
+$(window).bind('orientationchange resize', function(event){
+if (event.orientation) {
+    if (event.orientation == 'landscape') {
+    if (window.rotation == 90) {
+        rotate(this, -90);
+    } else {
+        rotate(this, 90);
+    }
+    }
+}
+});
+
+function rotate(el, degs) {
+iedegs = degs/90;
+if (iedegs < 0) iedegs += 4;
+transform = 'rotate('+degs+'deg)';
+iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
+styles = {
+    transform: transform,
+    '-webkit-transform': transform,
+    '-moz-transform': transform,
+    '-o-transform': transform,
+    filter: iefilter,
+    '-ms-filter': iefilter
+};
+$(el).css(styles);
+}
 const lenis = new Lenis()
 lenis.on('scroll', (e) => {})
 function raf(time) {
