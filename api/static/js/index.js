3bf8713636854fd4c3cfef11a9252f49e8f3e5de
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-18 21:10:02 +0530

    ...

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,11 +1,6 @@
-const lenis = new Lenis()
-gsap.registerPlugin(ScrollTrigger)
-
 lenis.on('scroll', (e) => {})
-
 function raf(time) {
     lenis.raf(time)
     requestAnimationFrame(raf)
 }
-
 requestAnimationFrame(raf)
