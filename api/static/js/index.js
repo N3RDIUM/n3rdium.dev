d10ed0facb4b761da03156986093a1e389486da3
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-18 21:52:18 +0530

    ...

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,6 +1,7 @@
 screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
 screen.lockOrientationUniversal("portrait-primary");
 screen.orientation.lock("portrait-primary");
+screen.lockOrientation("portrait-primary");
 const lenis = new Lenis()
 lenis.on('scroll', (e) => {})
 function raf(time) {
