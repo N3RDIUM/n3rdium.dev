a31e6422437c1e2dfeaf1dbb4dc284921c8b9fb4
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-20 10:07:30 +0530

    Added random backgrounds for different panels

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,9 +1,26 @@
-screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
-screen.lockOrientationUniversal("portrait-primary");
-screen.orientation.lock("portrait-primary");
-screen.lockOrientation("portrait-primary");
 const lenis = new Lenis()
-lenis.on('scroll', (e) => {})
+
+const panels = document.querySelectorAll('.panel')
+const variants = ['blue', 'green', 'purple', 'starfield']
+panels.forEach((panel) => {
+    const id = panel.id
+    const name = id.replace('panel', '')
+    const index = parseInt(name)
+    if (index % 2 == 0) {
+        let _string = variants[Math.floor(Math.random() * variants.length)].toString()
+        let _num = 1
+        if (_string == "purple") {
+            _num = Math.floor(Math.random() * 7) + 1
+        } else {
+            _num = Math.floor(Math.random() * 8) + 1
+        }
+        let bgname = 'background-' + _string + '-' + _num.toString()
+        panel.classList.add(bgname)
+    }
+})
+
+lenis.on('scroll', (e) => {
+})
 function raf(time) {
     lenis.raf(time)
     requestAnimationFrame(raf)
