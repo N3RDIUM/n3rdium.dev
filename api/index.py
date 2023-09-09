e280019f6d8359ddaaa0d8696a9a459d192c0a22
n3rdium <leenagajbhiye1003@gmail.com>
2023-09-09 18:47:02 +0530

    Added 'universe' page + better scrolldown animations

--- api/index.py
+++ api/index.py
@@ -8,21 +8,9 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
 def index():
     return render_template("index.html", title="Home")
-# @app.route('/contact/')
-# def contact():
-#     return render_template("contact.html", title="Contact")
-
-# Create the AMP version of the site
-# @app.route('/amp/')
-# def amp():
-#     return render_template("index_amp.html", title="Home")
-
-# @app.route('/robots.txt')
-# def robots():
-#     return str(open("./api/robots.txt").read())
-# @app.route('/sitemap.xml')
-# def sitemap():
-#     return str(open("./api/sitemap.xml").read())
+@app.route('/universe')
+def universe():
+    return render_template("universe.html", title="Universe")
 
 # Start the server with the 'run()' method
 if __name__ == '__main__':
