25547cd91b25ac0b482a05bd1b3d354b59de0902
n3rdium <n3rdium@gmail.com>
2024-05-21 12:48:00 +0530

    Update avatar for non-avif-supporting browsers

--- js/nav.js
+++ js/nav.js
@@ -137,3 +137,10 @@ function toggleNav() {
         })
     }
 }
+
+// If the browser supports avif
+var avif = new Image();
+avif.src = "data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A=";
+avif.onload = function () {
+	document.getElementById('havatar').setAttribute('src', "../img/n3rdium.avif")
+};
