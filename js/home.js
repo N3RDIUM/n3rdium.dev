395427fa41a5ac8007446560b86285cff7fe2af5
n3rdium <n3rdium@gmail.com>
2024-07-27 16:45:02 +0530

    Going for a more minimalist design

---
+++ js/home.js
@@ -0,0 +1,23 @@
+const lenis = new Lenis();
+
+const titles = ["00_INTR0", "01_AB0UT", "02_A$TR0", "03_C0DE"];
+
+lenis.on("scroll", (e) => {
+  let progress =
+    e.animatedScroll / (document.body.clientHeight - window.innerHeight);
+  let height = progress * window.innerHeight;
+  let idx = Math.round(e.animatedScroll / window.innerHeight);
+
+  document
+    .getElementById("progress")
+    .setAttribute("style", `height: ${height}px;`);
+  document.getElementById("progress-label").innerHTML = titles[idx];
+});
+
+function raf(time) {
+  lenis.raf(time);
+
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
