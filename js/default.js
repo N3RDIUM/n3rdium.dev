e68da10231c74823d57662c3438a37a0ff74a5cd
n3rdium <n3rdium@gmail.com>
2024-12-24 19:13:31 +0530

    Move random quotes to JS

--- js/default.js
+++ js/default.js
@@ -1,12 +1,32 @@
-console.log("42");
-
 const quotes = [
-	"I'd rather keep my face completely blank than wear a fake smile."
+    // Real quotes
+	"I'd rather keep my face completely blank than wear a fake smile.",
+
+    // Jokes
+    "Aim for the stars. You'll probably miss. Seriously, try it right now with a laser pointer!",
+    "I have a joke about HTTP, but you probably wouldn't GET it.",
+
+    // What?!
+    "The future sounds like Synthwave. The work needed to get there sounds like [neo]classical piano (with some violin).",
+    "The realisation that you're currently playing the most realistic open-world game known to mankind is just...",
 ];
 
-document.onload = () => {
-		let elements = document.getElementsByClassName("quote");
-	for(var element of elements) {
-		element
+function generateQuote() {
+    const index = Math.round(Math.random() * (quotes.length - 1))
+    const quote = quotes[index];
+
+    return `<div style="max-width: 420px">
+        <i>"${quote}"</i>
+        <p style="float: right">
+            - n3rdium
+        </p>
+    </div>
+    <br /><br />`
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const elements = document.getElementsByClassName("insert-quote-here");
+	for(const element of elements) {
+		element.innerHTML = generateQuote();
 	}
-};
+});
