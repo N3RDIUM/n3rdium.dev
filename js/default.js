98d80139150a6ae85879c3c72c9c6d00dd350cc1
n3rdium <n3rdium@gmail.com>
2025-01-21 17:59:06 +0530

    Contacts added + more quotes

--- js/default.js
+++ js/default.js
@@ -1,18 +1,21 @@
 const quotes = [
     // Real quotes
+    "Fourty Two.",
 	"I'd rather keep my face completely blank than wear a fake smile.",
     "The best time to start learning piano was several years ago. The second best time is now.",
+    "Mankind Was Born On Earth. It Was Never Meant To Die Here. - Cooper",
 
     // Jokes
     "Aim for the stars. You'll probably miss. Seriously, try it right now with a laser pointer!",
     "I have a joke about HTTP, but you probably wouldn't GET it.",
+    "In Order to Play the Joker, Joaquin Phoenix had to use both tabs AND spaces at once.",
 
     // What?!
     "The future sounds like Synthwave. The work needed to get there sounds like [neo]classical piano (with some violin).",
     "The realisation that you're currently playing the most realistic open-world game known to mankind is just...",
     "Who else is still distro-hoppin'?",
     "Fira Code has great ligatures <=> >= ||",
-    "NixOS + Hyprland + Astal + Neovim = <3"
+    "NixOS + Hyprland + Astal + Neovim = <3",
 ];
 
 function generateQuote() {
