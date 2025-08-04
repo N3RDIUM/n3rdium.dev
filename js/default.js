5650d950a205a3bc65a87eef2dc826ae7b194bf6
n3rdium <n3rdium@gmail.com>
2025-08-04 21:03:39 +0530

    better organization

--- js/default.js
+++ js/default.js
@@ -10,7 +10,6 @@ const nav = `<a class="nav-username thefont" href="/">N3RDIUM</a>
 <div class="nav-links">
     <a class="nav-link thefont" href="/about/">About</a>
     <a class="nav-link thefont" href="/work/">Work</a>
-    <a class="nav-link thefont" href="/social/">Social</a>
     <a class="nav-link thefont" href="/contact/">Contact</a>
 </div>`
 
