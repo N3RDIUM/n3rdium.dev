c8c88da14bea225d74356ff4392bdf017eb0c3cd
n3rdium <n3rdium@gmail.com>
2026-03-01 04:55:54 +0530

    fix: better include repo handling/logging

--- scripts/process_includes.py
+++ scripts/process_includes.py
@@ -1,32 +1,65 @@
 import os
 import json
 import shutil
+import subprocess
 
 with open("includes.json", "r") as f:
     includes: dict[str, str] = json.load(f)
 
-if os.path.exists("includes/"):
-    shutil.rmtree("includes")
+root = os.path.dirname(os.path.dirname(__file__))
 
 def process_includes():
     for slug, repo in includes.items():
         print(f"processing include {slug}")
         path: str = os.path.join("includes/", slug)
         dist_path: str = os.path.join("dist/", slug)
-        os.makedirs(path, exist_ok=True)
 
-        # No error handling for this because I intend it to behave as an error 
-        # instead of crashing the entire build.
-        _ = os.system(f"git clone {repo} {path}")
+        fresh_clone = False
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+            # I intend it to behave as a warning instead of crashing the build.
+            print(f"> git clone {repo} {path} -q")
+            try:
+                _ = subprocess.check_output(
+                    ["git", "clone", repo, path, "-q"],
+                    text=True,
+                )
+                fresh_clone = True
+            except subprocess.CalledProcessError as e:
+                print(f"> failed to clone {repo}: {e}. skipping this include.")
+                continue
+        else:
+            print(f"# {path} already exists, skipping clone.")
 
         # TODO copy JS/CSS from (if exists) from repo to src/
 
         # TODO if the repos already exist (eg. on a dev machine) just git pull
 
-        print(f"chdir to {path}")
+        print(f"> chdir {path}")
         os.chdir(path)
-        _ = os.system("./dist.sh")
-        os.chdir(os.path.dirname(os.path.dirname(__file__)))
-        print("chdir reset")
+
+        if not fresh_clone:
+            print("> git pull -q")
+            try:
+                _ = subprocess.check_output(
+                    ["git", "pull", "-q"],
+                    text=True
+                )
+            except subprocess.CalledProcessError as e:
+                print(f"> failed to pull changes: {e}. skipping this include.")
+                continue
+
+        print("> ./dist.sh")
+        try:
+            _ = subprocess.check_output(
+                ["./dist.sh"],
+                text=True
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"> dist.sh failed: {e}. skipping this include.")
+
+        print(f"< chdir {root}")
+        os.chdir(root)
 
         _ = shutil.copytree(os.path.join(path, "dist/"), dist_path)
