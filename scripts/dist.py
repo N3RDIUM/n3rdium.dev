5256fd78881f3e5d934b0ffddd3cb6e1a40ff76b
n3rdium <n3rdium@gmail.com>
2026-03-02 19:47:01 +0530

    feat: permafrost integration

--- scripts/dist.py
+++ scripts/dist.py
@@ -1,28 +1,26 @@
 import os
 import shutil
 
-from .process_includes import process_includes
-from .process_metadata import process_metadata
-from .postprocess_dev import dev_remove
-from .postprocess_inline import inline, load_css, load_js
-from .postprocess_minify import minifier
+from .metadata import process_metadata
+from .dev import dev_remove
+from .inline import inline, load_css, load_js
+from .minify import minifier
 
 if os.path.exists("./dist/"):
     shutil.rmtree("./dist/")
 
 _ = shutil.copytree("./src", "./dist")
-
-# build
-process_includes()
-
 os.chdir("dist")
-process_metadata()
 
-# post-processing
-dev_remove()
+# preload
 load_css("css")
 load_js("js")
+
+# build
+process_metadata()
+dev_remove()
 inline()
 minifier()
+
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
 
---
+++ scripts/dist.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+python -m pip install -U -r requirements.txt
+
+if [ ! -d "permafrost" ]; then
+    git clone https://github.com/n3rdium/permafrost.git
+else
+    cd permafrost || exit 1
+    git pull
+    cd ..
+fi
+
+python -m pip install -U -r permafrost/requirements.txt
+python -m permafrost.main
+python -m scripts.dist
