83bb27cf1b5114cf34468915a32f392b819928e1
n3rdium <n3rdium@gmail.com>
2026-03-04 18:23:56 +0530

    feat: desktop nav + 404 page

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,3 +1,40 @@
+const NAV_HTML = `
+<a class="nav-gohome" href="/">
+    N3RDIUM
+</a>
+<button class="nav-drawer-button" id="nav-drawer-button">
+    <img 
+        src="/assets/icons/menu.svg"
+        alt="Menu"
+        width=28
+        height=28
+    />
+</button>
+<div class="drawer drawer-closed" id="drawer"></div>
+<div class="desktop-nav">
+    <div class="desktop-nav-link">
+        <a class="desktop-nav-link-text" href="/about/">About</a>
+        <div class="desktop-nav-drop">
+            <a class="desktop-nav-item" href="/contact/">Contact</a>
+            <a class="desktop-nav-item" href="/stack/">Stack</a>
+        </div>
+    </div>
+    <div class="desktop-nav-link">
+        <a class="desktop-nav-link-text" href="/work/">Work</a>
+        <div class="desktop-nav-drop">
+            <a class="desktop-nav-item" href="/projects/">Projects</a>
+            <a class="desktop-nav-item" href="/astro/">Astronomy</a>
+            <a class="desktop-nav-item" href="/piano/">Piano</a>
+        </div>
+    </div>
+    <div class="desktop-nav-link">
+        <a class="desktop-nav-link-text" href="/blog/">Blog</a>
+    </div>
+    <div class="desktop-nav-link">
+        <a class="desktop-nav-link-text" href="/notes/">Notes</a>
+    </div>
+</div>`;
+
 function toggleDrawer() {
     let drawer = document.getElementById("drawer");
     let container = document.body;
@@ -11,6 +48,12 @@ function toggleDrawer() {
 }
 
 window.onload = () => {
+    let nav_el = document.createElement("nav");
+    nav_el.id = "nav";
+    nav_el.classList.add("nav-container");
+    nav_el.innerHTML = NAV_HTML;
+    document.body.appendChild(nav_el);
+
     let drawer_toggle = document.getElementById("nav-drawer-button");
     drawer_toggle.onclick = toggleDrawer;
 }
