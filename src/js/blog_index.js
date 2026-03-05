9e6f01f69bb7c78bdc3cd51e53875a10166e164c
n3rdium <n3rdium@gmail.com>
2026-03-05 06:13:58 +0530

    feat: blog page

--- src/js/blog_index.js
+++ src/js/blog_index.js
@@ -1,6 +1,29 @@
 var content = {};
 
+function blog_post_card({ relpath, title, description, tags, readtime }) {
+    let tags_html = "";
+    if(tags != null) {
+        for(var tag of tags) {
+            tags_html += `<div class="tag">${tag}</div>`;
+        }
+    }
+
+    return `<a class="blog-post" href="${relpath}">
+    <button class="post responsive-border">
+        <h2 class="heading">${title}</h2>
+        <div class="description">${description}</div>
+        <div class="metadata">
+            <div class="tags">${tags_html}</div>
+            <div class="readtime">${readtime}</div>
+        </div>
+    </button>
+</a>`;
+}
+
 fetch("/blog/index.json").then(async res => {
+    const cards_el = document.getElementById("blog_post_cards")
     content = await res.json().catch(() => alert("/blog/index.json: Bad JSON format!"));
-    console.log(content);
-}).catch(() => alert("Couldn't load blog posts! Please try again."));
+    for(var post of content.posts) {
+        cards_el.innerHTML += blog_post_card(post);
+    }
+});
